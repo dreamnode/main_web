b1a8236ca9edfbbb038c0afb7c12a070a478817f
Sirkays <attehkayode2@gmail.com>
2021-04-07 17:20:47 +0100

    work on the form on dreamnode.html

--- dreamnode/forms.py
+++ dreamnode/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class ContactFormEmail(forms.Form):
 	from_email = forms.EmailField(required=True)
 	subject = forms.CharField(required=True)
-	message = forms.CharField(widget=forms.Textarea,required=True)
+	message = forms.CharField(label="", widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter message', 'rows':'8', 'cols':'40'}), required=True)
  	
 
  	
