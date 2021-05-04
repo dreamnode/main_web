57a465f922b92feb6d2e531d6c11b70a563738ef
Sirkays <attehkayode2@gmail.com>
2021-05-04 14:36:54 +0100

    made changes to settings

--- dreamnode_site/settings.py
+++ dreamnode_site/settings.py
@@ -146,7 +146,6 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 
-
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
@@ -158,12 +157,8 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_BUCKET_ACL = "public-read"
 AWS_QUERYSTRING_AUTH = False
-AWS_S3_REGION_NAME = "your-bucket-region"
+AWS_S3_REGION_NAME = "EU (Paris) eu-west-3"
 
-
-
-AWS_AUTO_CREATE_BUCKET = True
-AWS_STORAGE_BUCKET_NAME = "my_bucket"
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
