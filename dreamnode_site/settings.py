fb7f993cd7c30feb1d9ce35962f8aef445ccf091
Sirkays <attehkayode2@gmail.com>
2021-05-05 13:16:31 +0100

    used coreyms settings

--- dreamnode_site/settings.py
+++ dreamnode_site/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_URL = '/static/'
 
 #MEDIA_URL = 'media/'
@@ -135,6 +135,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 
@@ -150,11 +155,34 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+
+
+
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+'''
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS= {'CacheControl':'max-age=86400'}
 
 
 
+
+
+
+
+
+
+
+'''
+
+
+
+'''
 #AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = "public-read"
 #AWS_QUERYSTRING_AUTH = False
@@ -171,6 +199,6 @@ DEFAULT_FILE_STORAGE = 'dreamnode_site.storages.MediaStore'
 
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #django_heroku.settings(locals())
-
+'''
 
 
