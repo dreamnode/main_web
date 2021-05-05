8fa436e017cb4a909a006941a5955638fb66575a
Sirkays <attehkayode2@gmail.com>
2021-05-05 13:18:59 +0100

    used public-read for acl

--- dreamnode_site/settings.py
+++ dreamnode_site/settings.py
@@ -161,7 +161,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
