1192c57dbe734675a921cfb9241ab0191d311e9a
Sirkays <attehkayode2@gmail.com>
2021-05-06 22:53:24 +0100

    modified settings.py2

--- dreamnode/admin.py
+++ dreamnode/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import our_work, Post,branding,ads,dreamnode_social,other
+from .models import our_work, Post,branding,ads,dreamnode_social,others
 
 
 admin.site.register(our_work)
